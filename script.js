55f378afdae3539fe8891185b755ec86b8712255
xpokr300 <pokorn.richard@gmail.com>
2023-11-23 15:42:46 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function returnBinary (form) {
+    try{
+        //input variable
+        let inputValue = form.inputbox.value;
+        //output variable
+        let outputValue = '';
+        //validation for data type
+        if(Number.isInteger(Number(inputValue))){
+            //result variable - this is the base
+            let result = inputValue;
+            //rests - results are 0/1. It is stored as s string concatenation
+            let rest = '';
+            //loop iteration 
+            while(result>0){
+                rest += result % 2;
+                result  = Math.floor(result/2);
+            }
+            //set output -change the order of numbers in rest
+            for(let i = rest.length -1; i>-1;i--){
+                outputValue += rest[i];
+            }
+        //print the result     
+        document.getElementById('result').innerHTML = 'Výsledek je: ' + outputValue; 
+        //log the console
+        console.log("Proběhlo prověření čísla "+inputValue + ". Výsledek je: " + outputValue);
+        }
+        else{
+            alert("Toto není celé číslo. Zkuste to znovu.");
+            return false;
+        }
+    }catch(error){
+        alert('Šéfe, mně se asi něco nepovedlo')
+        console.log("Proběhlo prověření čísla "+inputValue + ". Výsledkem je chyba.");
+    }
+
+    }
